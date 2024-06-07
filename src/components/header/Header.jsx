7eea98971db291b5aab7b18e8ffac1af2b39dfe4
Art Vardanyan <akvardanyan99@gmail.com>
2024-06-07 16:45:36 -0400

    header.css, header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import './header.css';
 
 const Header = () => {
+
+    const [Toggle, showMenu] = useState(false);
+
     return (
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">Vardanyan</a>
 
-                <div className="nav__menu">
-                    <ul className="nac__list grid">
+                <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
+                    <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link">
+                            <a href="#home" className="nav__link active-link">
                                 <i className="uil uil-estate nav__icon"></i>Home
                             </a>
                         </li>
@@ -45,11 +49,11 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <i className='uil uil-times nav__close'></i>
+                    <i className='uil uil-times nav__close' onClick={() => showMenu(!Toggle)}></i>
                 </div>
 
-                <div className="nav__toggle">
-                    <i class='uil uil-apps'></i>
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i className='uil uil-apps'></i>
                 </div>
             </nav>
         </header>
