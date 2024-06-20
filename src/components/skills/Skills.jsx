6ecff1ae1ac9b6240bff0cb9083abe326bee9644
Art Vardanyan <akvardanyan99@gmail.com>
2024-06-20 12:13:26 -0400

    wip

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,7 +6,7 @@ import Backend from './Backend';
 const Skills = () => {
     return (
         <section className="skills section #sills">
-            <h2 className="section__title">Skills</h2>
+            <h2 className="section__title" id='skills'>Skills</h2>
             <span className="section__subtitle">My Technical Level</span>
 
             <div className="skills__container container grid">
