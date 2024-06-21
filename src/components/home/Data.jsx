8acfea63340e09f2ed9ea171b4d010ce0874b5ae
Art Vardanyan <akvardanyan99@gmail.com>
2024-06-21 12:25:32 -0400

    wip

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -82,6 +82,25 @@ const Data = () => {
                     ></path>
                 </svg>
             </a>
+
+            <a href="#contact" className="button button--flex">
+                View My Work
+                <svg
+                    className="button__icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                </svg>
+            </a>
         </div>
     );
 }
