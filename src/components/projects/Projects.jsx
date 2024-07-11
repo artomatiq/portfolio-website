93f24b1a08940a6aa23f3b5450b1de7c7459a259
Art Vardanyan <akvardanyan99@gmail.com>
2024-07-11 19:30:56 -0400

    wip

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -20,30 +20,35 @@ const Portfolio = () => {
             track.dataset.mouseDownAt = e.clientX;
         }
     
-        window.onmouseup = () => {
-            //stop the slider when the mouse is up
-            track.dataset.mouseDownAt = '0';
-
-            //start slider where left off when mouse is clicked again
-            track.dataset.prevPercentage = track.dataset.percentage;
-
-            console.log(track.dataset.mouseDownAt);
-        }
-    
         window.onmousemove = e => {
             //if mouse is not clicked, do not move slider
             if(track.dataset.mouseDownAt === "0") return;
     
             //convert mouse down distance to percentage by which slider should move
             const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-            const maxDelta = window.innerWidth / 2;
-            const percentage = (mouseDelta / maxDelta) * -100;
+            const maxDelta = window.innerWidth / 1.5;
+            const percentage = (mouseDelta / maxDelta) * (100);
 
-            const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+            let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+            // if (nextPercentage > -50) {
+                nextPercentage = Math.max(nextPercentage, 0)
+            // } else {
+                nextPercentage = Math.min(nextPercentage, 100)
+            // }
+            
+
     
             track.dataset.percentage = nextPercentage;
     
-            track.style.transform = `translate(${nextPercentage}%, 10%)`;
+            track.style.transform = `translate(-${nextPercentage}%)`;
+        }
+
+        window.onmouseup = () => {
+            //stop the slider when the mouse is up
+            track.dataset.mouseDownAt = '0';
+
+            //start slider where left off when mouse is clicked again
+            track.dataset.prevPercentage = track.dataset.percentage;
         }
     }, [])
 
@@ -57,10 +62,16 @@ const Portfolio = () => {
 
             <div id='projects'>
                 <div id='image-track' data-mouse-down-at='0' data-prev-percentage='0'>
-                    <img className='project' src={image1} alt='project snapshot' draggable="false" />
-                    <img className='project' src={image2} alt='project snapshot' draggable="false" />
-                    <img className='project' src={image3} alt='project snapshot' draggable="false" />
-                    <img className='project' src={image4} alt='project snapshot' draggable="false" />
+                    <img className='project one' src={image1} alt='project snapshot' draggable="false" />
+                    <img className='project two' src={image2} alt='project snapshot' draggable="false" />
+                    <img className='project three' src={image3} alt='project snapshot' draggable="false" />
+                    <img className='project four' src={image4} alt='project snapshot' draggable="false" />
+                    <img className='project five' src={image1} alt='project snapshot' draggable="false" />
+                    <img className='project six' src={image2} alt='project snapshot' draggable="false" />
+                    <img className='project seven' src={image3} alt='project snapshot' draggable="false" />
+                    <img className='project eight' src={image4} alt='project snapshot' draggable="false" />
+                    <img className='project nine' src={image1} alt='project snapshot' draggable="false" />
+                    <img className='project ten' src={image2} alt='project snapshot' draggable="false" />
                 </div>
             </div>
 
