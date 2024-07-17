e4ad7fe41d694cf41c06fbbefcd17c5e5e5cb035
Art Vardanyan <akvardanyan99@gmail.com>
2024-07-17 18:52:43 -0400

    scroll ids

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                         </li>
 
                         <li className="nav__item">
-                            <a href="#projects" className="nav__link">
+                            <a href="#portfolio" className="nav__link">
                                 <i className="uil uil-scenery nav__icon"></i>Projects
                             </a>
                         </li>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -201,7 +201,7 @@ const Portfolio = () => {
     
 
     return (
-        <section className="portfolio section">
+        <section className="portfolio section" id='portfolio'>
             <h2 className="section__title">Projects</h2>
             <span className="section__subtitle">Things I've Built</span>
 
