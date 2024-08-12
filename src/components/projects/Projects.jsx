c9962317db43fd448fcdfc9322da21ff06ee85f9
Art Vardanyan <akvardanyan99@gmail.com>
2024-08-12 16:22:32 -0400

    wip

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import './projects.css';
 
-import image1 from '../../assets/img1.png'
+import image1 from '../../assets/ccs-expedited.png'
 import image2 from '../../assets/img2.png'
 import image3 from '../../assets/img3.png'
-import image4 from '../../assets/img4.png'
+
 
 import tweetyBird from '../../assets/trimmed.mp4'
 
@@ -52,6 +52,23 @@ const Portfolio = () => {
         <section className="portfolio section" id='portfolio'>
             <h2 className="section__title hide"><span className='hide'>My Projects</span></h2>
 
+            <div className='project-container'>
+                <div className="project-image">
+                    <img className='project hide' src={image1} alt='project snapshot' draggable="false" />
+                </div>
+                <div className="project-info">
+                    <h2 className='project-title hide'>
+                        Courier Admin Dash
+                        <h2>in progress...</h2>
+                    </h2>
+                    <p className='project-description ccs-expedited hide'>An admin panel for Carolina's Courier Services. The frontend is built with HTML, CSS, JavaScript and React. On the backend, we have Node, Express and MySQL. The driver version of the app implements clock-in functionality, generating time logs in a MySQL database, which the admin then uses for gauging activity and generating reports in real time. The admin can also assign hourly rates to drivers and easily manage payroll.</p>
+                    <div className="project-buttons hide">
+                        <a href="https://github.com/artomatiq" className="project-button hide" rel="noreferrer" target="_blank">CODE   <i className="uil uil-github-alt"></i></a>
+                        <a href="https://github.com/artomatiq" className="project-button hide" rel="noreferrer" target="_blank">DEMO   <i className="uil uil-eye"></i></a>
+                    </div>
+                </div>
+            </div>
+
             <div id='projects'>
                 <div className='project-container'>
                     <div className="project-image">
@@ -59,24 +76,10 @@ const Portfolio = () => {
                     </div>
                     <div className="project-info">
                         <h2 className='project-title hide'>Tweety Bird: The X-Scape</h2>
-                        <p className='project-description hide'>A Twitter vs Elon Musk's X rivalry themed clone of the popular game Flappy Bird—developed using HTML, CSS and JavaScript.</p>
+                        <p className='project-description tweety-bird hide'>A Twitter vs Elon Musk's X rivalry themed clone of the popular game Flappy Bird—developed using HTML, CSS and JavaScript.</p>
                         <div className="project-buttons hide">
                             <a href="https://github.com/artomatiq/tweetie-bird" className="project-button hide" rel="noreferrer" target="_blank">CODE   <i className="uil uil-github-alt"></i></a>
                             <a href="https://tweety-bird.vercel.app/" className="project-button hide" rel="noreferrer" target="_blank">DEMO   <i className="uil uil-eye"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='project-container'>
-                    <div className="project-image">
-                        <img className='project hide' src={image2} alt='project snapshot' draggable="false" />
-                    </div>
-                    <div className="project-info">
-                        <h2 className='project-title hide'>Tweety Bird: The X-Scape</h2>
-                        <p className='project-description hide'>A Twitter vs Elon Musk's X rivalry themed clone of the popular game Flappy Bird—developed using HTML, CSS and JavaScript.</p>
-                        <div className="project-buttons hide">
-                            <a href="https://github.com/artomatiq" className="project-button hide" rel="noreferrer" target="_blank">CODE   <i className="uil uil-github-alt"></i></a>
-                            <a href="https://github.com/artomatiq" className="project-button hide" rel="noreferrer" target="_blank">DEMO   <i className="uil uil-eye"></i></a>
                         </div>
                     </div>
                 </div>
