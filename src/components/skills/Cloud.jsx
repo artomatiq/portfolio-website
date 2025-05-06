31bb7fab54ad8d2174aac383477a04668b23a2ce
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-06 13:07:57 -0400

    wip

--- src/components/skills/Cloud.jsx
+++ src/components/skills/Cloud.jsx
@@ -54,14 +54,6 @@ const Cloud = () => {
                             {/* <span className="skills__level">proficient</span> */}
                         </div>
                     </div>
-
-                    {/* <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-                        <div>
-                            <h3 className="skills__name">Git</h3>
-                            <span className="skills__level">proficient</span>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
